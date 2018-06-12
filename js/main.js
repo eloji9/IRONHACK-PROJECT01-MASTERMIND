cda97cb7ed062babdd9e45194a4cfa0b2f906548
eloji9 <eloji9@hotmail.com>
2018-06-12 14:48:16 +0200

    Day2-push2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,54 @@
 $(document).ready(function(){
 
-    let selectedColor = "";
-    // let bGround = "";
+    var guess = 0;
+    var selectedColor = "";
+    var bGround = "rgba(0, 0, 0, 0) linear-gradient(grey, rgb(218, 243, 216)) repeat scroll 0% 0% / auto padding-box border-box";
 
+
+
+    var tempRay = $(".guess-pegs");
+    // console.log(tempRay);
+    var guessBoxArray = [];
+    for(var i = 9; i >= 0; i--){
+        guessBoxArray.push(tempRay[i]);
+    }
+
+    for(var i = 0; i < 10; i++){
+        var guessArray = guessBoxArray[i].getElementsByClassName("guess-peg");
+        for(var j = 0; j < 4; j++){
+            $(guessArray[j]).attr("id",`g-${i}-${j}`)
+        }
+    }
+
+    $(".submit-btn").click(function(){
+        $(".active").removeClass("active");
+        guess++;
+        for(var i = 0; i < 4; i++){
+            $(`#g-${guess}-${i}`).addClass("active");
+        }
+    });
 
     $(".selector-inner").click(function(){
         $(".selector-outer").css("background-color","brown");
-        let peg = ($(this).parent())[0];
-        let selectedColor =
+        var peg = ($(this).parent())[0];
+        selectedColor =
         $(this).css("background-color");
         $(peg).css("background-color",selectedColor);
-        console.log(this);
     });
-
-    console.log(selectedColor);
 
     $('.guess-peg').click(function() {
-        console.log(this);
-        // $(this).css('background', 'none');
-        let selectedColor =
-        $(this).css('background-color');
-        // $(this).css('border', '2px solid white');
+        if ($(this).hasClass("active")){
+            var number = parseInt($(this).css("border"));
+            if(number === 1){
+                $(this).css("background","none");
+                $(this).css("background-color",selectedColor);
+                $(this).css("border","2px solid white");
+            } else {
+                $(this).css("background",bGround);
+                $(this).css("border","1px solid white");
+            }
+        }
     });
+
 
 });
