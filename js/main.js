b7dca53bdf08e0796a272070f3baa7544286138b
eloji9 <eloji9@hotmail.com>
2018-06-12 17:22:23 +0200

    Day2-push3

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     var guess = 0;
     var selectedColor = "";
     var bGround = "rgba(0, 0, 0, 0) linear-gradient(grey, rgb(218, 243, 216)) repeat scroll 0% 0% / auto padding-box border-box";
-
-
-
+    $(".submit-btn").hide();
+    var clickCount = 0;
+    var isSelected = false;
+    var answerRay = makeAnswer();
     var tempRay = $(".guess-pegs");
-    // console.log(tempRay);
     var guessBoxArray = [];
     for(var i = 9; i >= 0; i--){
         guessBoxArray.push(tempRay[i]);
@@ -20,35 +20,83 @@ $(document).ready(function(){
         }
     }
 
+    var masterGuessArray = [[-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1],
+                            [-1,-1,-1,-1]];
+
     $(".submit-btn").click(function(){
         $(".active").removeClass("active");
+        console.log(masterGuessArray[guess]);
+        console.log(answerRay);
         guess++;
         for(var i = 0; i < 4; i++){
             $(`#g-${guess}-${i}`).addClass("active");
         }
+        $(".submit-btn").hide();
     });
 
     $(".selector-inner").click(function(){
+        isSelected = true;
         $(".selector-outer").css("background-color","brown");
         var peg = ($(this).parent())[0];
-        selectedColor =
-        $(this).css("background-color");
+        selectedColor = $(this).css("background-color");
         $(peg).css("background-color",selectedColor);
     });
 
     $('.guess-peg').click(function() {
-        if ($(this).hasClass("active")){
-            var number = parseInt($(this).css("border"));
-            if(number === 1){
-                $(this).css("background","none");
-                $(this).css("background-color",selectedColor);
-                $(this).css("border","2px solid white");
-            } else {
-                $(this).css("background",bGround);
-                $(this).css("border","1px solid white");
+        if (isSelected){
+            if ($(this).hasClass("active")){
+                var number = parseInt($(this).css("border"));
+                if(number === 1){
+                    $(this).css("background","none");
+                    $(this).css("background-color",selectedColor);
+                    $(this).css("border","2px solid white");
+                    var coord = $(this).attr("id");
+                    updateMasterArray(selectedColor, coord);
+                    clickCount++;
+                    if(clickCount === 4){
+                        $(".submit-btn").show();
+                        clickCount = 0;
+                    };
+                } else {
+                    $(this).css("background",bGround);
+                    $(this).css("border","1px solid white");
+                    // updateMasterArray(selectedColor, coord);
+                    clickCount--;
+                }
             }
         }
     });
 
+    function makeAnswer(){
+        var ray = [];
+        for(var i = 0; i < 4; i++){
+            ray.push(Math.floor(Math.random() * 6));
+        }
+        return ray;
+    }
 
+    function updateMasterArray(col, xy) {
+        var ray = xy.split('-');
+        var x = ray[1];
+        var y = ray[2];
+        masterGuessArray[x][y] = makeColorANumber(col);
+        console.log(masterGuessArray);
+      }
+
+    function makeColorANumber(col){
+        if(col === 'rgb(255, 0, 0)') return 0;
+        if(col === 'rgb(255, 255, 0)') return 1;
+        if(col === 'rgb(0, 0, 255)') return 2;
+        if(col === 'rgb(0, 255, 0)') return 3;
+        if(col === 'rgb(0, 0, 0)') return 4;
+        if(col === 'rgb(255, 255, 255)') return 5;
+    }
 });
